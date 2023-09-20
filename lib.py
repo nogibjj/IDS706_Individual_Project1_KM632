d6d5cab1e2728a863b0a6f611d85316cb195ec4f
Haliunaa Munkhuu <42748002+khaliun20@users.noreply.github.com>
2023-09-20 23:39:31 +0000

    fix size

--- lib.py
+++ lib.py
@@ -52,7 +52,7 @@ def create_graph2(df):
                    ]
     df['Cost Range'] = pd.cut(df['Cost Per Year'], bins=cost_bins, labels=cost_labels)
     cost_range_counts = df['Cost Range'].value_counts()
-    plt.figure(figsize=(6, 6))
+    plt.figure(figsize=(8, 8))
     plt.pie(cost_range_counts, 
             labels=cost_labels, 
             autopct='%1.1f%%', 
